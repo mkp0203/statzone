0786c15c0423fea602862bde07a49e44d4a11c6b
Brendon Esperancilla <brendon.esp@gmail.com>
2018-11-28 20:13:44 -0500

    add .env and blank json

--- statzone.sql
+++ statzone.sql
@@ -6,8 +6,16 @@ USE statzone;
 CREATE TABLE players(
   id INTEGER(11) AUTO_INCREMENT NOT NULL,
   playerName VARCHAR(100),
+  playerAliasTableId INTEGER(11),
   PRIMARY KEY (id)
-);
+)
+
+CREATE TABLE aliases(
+    id INTEGER(11) AUTO_INCREMENT NOT NULL,
+    playerId INTEGER(11) NOT NULL,
+    alias VARCHAR(100),
+    lastUsed VARCHAR(100)
+)
 
 CREATE TABLE games(
     id INTEGER(3) NOT NULL,
@@ -28,6 +36,9 @@ CREATE TABLE stats_LeagueOfLegends(
     id INTEGER(11) AUTO_INCREMENT NOT NULL,
     playerName VARCHAR(100),
     playerId INTEGER(11) NOT NULL,
+    playerLevel INTEGER(3) NOT NULL,
+    playerRank VARCHAR(50) NOT NULL,
+    playerLP INTEGER(3) NOT NULL,
     gameName VARCHAR(100),
     gameId INTEGER(3) NOT NULL,
     win/loss FLOAT(3) NOT NULL,
@@ -42,7 +53,8 @@ CREATE TABLE matchHistory_LeagueOfLegends(
     matchWin BOOLEAN NOT NULL,
     matchDuration VARCHAR(50) NOT NULL,
     kills INTEGER(3) NOT NULL,
-
+    deaths INTEGER(3) NOT NULL,
+    assists INTEGER(3) NOT NULL
 )
 
 CREATE TABLE champions_LeagueOfLegends(
