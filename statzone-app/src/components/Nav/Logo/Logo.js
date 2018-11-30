519f3dac4f158e26f33507ac1258ebec81ba6569
gpoell <gpoell.gp@gmail.com>
2018-11-29 23:03:02 -0500

    updated background and logo

--- statzone-app/src/components/Nav/Logo/Logo.js
+++ statzone-app/src/components/Nav/Logo/Logo.js
@@ -2,11 +2,11 @@ import React from "react";
 import "./Logo.css";
 
 // you can do css in the css file or do it specifically in here
-// const styles = {
-//     image: {
-        
-//     }
-// }
+const styles = {
+    image: {
+        height: 100
+    }
+}
 
 const Logo = (props) => (
     // eventually we will need to pass the props back down to this component but for now we can manage everything here
@@ -15,6 +15,7 @@ const Logo = (props) => (
         //alternatively we can do style={styles.images}
         src={props.image}
         alt="League of Legends"
+        style={styles.image}
     />
 )
 
