26766754d9fe2eed18d927ce0b6a663f1d1d3503
gpoell <gpoell.gp@gmail.com>
2018-11-30 17:45:32 -0500

    removing API key

--- statzone-app/src/utils/LeagueAPI.js
+++ statzone-app/src/utils/LeagueAPI.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 const BASEURL = "https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/";
-const APIKEY = "?api_key=RGAPI-a5bf1f05-32d0-4daf-9a2d-52152fa7e2cb";
 
 // Export an object with a "search" method that searches the Giphy API for the passed query
 const API = {
